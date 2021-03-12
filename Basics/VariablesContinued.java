9b640a0fdfbda8004a5fab81885ab27d5e6d85d3
robertelias <robert.elias@live.com>
2021-03-12 15:23:11 -0800

    updates

--- Basics/VariablesContinued.java
+++ Basics/VariablesContinued.java
@@ -1,5 +1,14 @@
-public interface VariablesContinued {
-    public void name() {
+public class VariablesContinued {
+    public static void main(String[] args) {
+        int var = 10000000;
+        long bigNumber = 1000000000000L;
+        byte reallySmallNumber = -128;
+        double decimalVar = 394.003;
+        boolean decision = false;
+        char letter = '$';
         
+        System.out.println(var + bigNumber - reallySmallNumber * decimalVar);
+        System.out.println(decision);
+        System.out.println(letter);
     }
 }
