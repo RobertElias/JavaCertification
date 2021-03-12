d9b7fb846c71de54d285e7f5074a4c7e07bc480d
robertelias <robert.elias@live.com>
2021-03-12 15:38:47 -0800

    created array file

---
+++ Basics/LearningArrays.java
@@ -0,0 +1,20 @@
+/**
+ * LearningArrays
+ */
+public class LearningArrays {
+         public static void main(String[] args) {
+
+            int [] values = new int[100];
+            values[0] = 1000;
+            values[99] = 93432;
+             System.out.println(values[99]);
+
+             String [] words = new String[3];
+             words[0] = "My";
+             words[1] = "Name";
+             words[2] = "is";
+             
+             System.out.println(words[0]+ " " + words[1] + " " + words[2] + " " + "Robert Elias");
+         }
+    
+}
