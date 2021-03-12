ae51138ae6c736ca298b83b070b817f13bf600d5
robertelias <robert.elias@live.com>
2021-03-12 13:28:28 -0800

    make basics directory

---
+++ Basics/Variables.java
@@ -0,0 +1,19 @@
+class Variables {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+		
+		int x;
+		x = 34;
+		
+		System.out.println(x);
+		
+		x = 334;
+		
+		System.out.println(x);
+		
+		String words;
+		words = "This is a sentence.";
+		
+		System.out.println(words);
+    }
+}
---
+++ Basics/VariablesContinued.java
@@ -0,0 +1,5 @@
+public interface VariablesContinued {
+    public void name() {
+        
+    }
+}
